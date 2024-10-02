602bd85427913e86a19efd312a909e7fefedba12
Renao Chetri <63331185+ChhetriPrem@users.noreply.github.com>
2024-10-02 19:30:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,29 +4,24 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 
 const authRoutes = require("./api/auth");
-const todoRoutes = require("./api/todos"); // Assuming you have todo routes as well
+const todoRoutes = require("./api/todos");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 6969;
 
-// Middleware
 app.use(express.json());
 app.use(cookieParser());
 const mongoUri = process.env.MONGO_URI;
-// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// MongoDB connection (Replace with your actual MongoDB connection string)
 mongoose
-  .connect(
-    mongoUri)
+  .connect(mongoUri)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Use routes
-app.use("/api/auth", authRoutes); // All auth routes (signup, signin) go under /api/auth
-app.use("/api/todos", todoRoutes); // Example for todos if you have them
+app.use("/api/auth", authRoutes);
+app.use("/api/todos", todoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
